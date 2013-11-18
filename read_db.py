b0e1b9c9811b130d2a60810e9599c0b5475deb18
FreyrLin <fan.lin@tieto.com>
2013-11-18 13:57:29 +0800

    add resize image functions to read_db.py, it's for print convenience only

--- read_db.py
+++ read_db.py
@@ -2,7 +2,7 @@
 #=============================================================================
 #     FileName: read_db.py
 #         Desc: read baicizhan db folder, create resource files for giveaword
-#      Version: 3.0.0
+#      Version: 4.0.0
 #=============================================================================
 '''
 
@@ -10,6 +10,7 @@ import sqlite3
 import json
 import os, glob, shutil
 import re
+from PIL import Image
 
 # word list
 word_list = []
@@ -92,7 +93,7 @@ def create_folder(folder):
     try:
         os.mkdir(folder)
     except OSError or FileExistsError:
-        print('[OMIT] folder {} already exist.'.format(folder))
+        #print('[OMIT] folder {} already exist.'.format(folder))
         return False
     return True
 
@@ -104,22 +105,34 @@ def copy_files(folder, word, field):
     except OSError:
         print('[FAILED] {} -> {}'.format(word[field], target_path))
 
+def resize_all_images(src_folder, tar_folder, width):
+    for file in os.listdir(src_folder):
+        image = Image.open('{}/{}'.format(src_folder, file))
+        size = image.size
+        image = image.resize((width, int(float(width) * size[1] / size[0])),
+                             Image.BILINEAR)
+        image.save('{}/{}'.format(tar_folder, file))
+
 # Convert dictionary resource
 def convert_dict_recource():
     if create_folder('book/book_images'):
         for word in word_list: # copy image file
             copy_files('book/book_images', word, T_IMAGEPATH)
-    if create_folder('book/book_pronounce'):
-        for word in word_list: # copy word pronounce file
-            copy_files('book/book_pronounce', word, T_WORDVIDEO)
-    if create_folder('book/book_sentence'):
-        for word in word_list: # copy sentence pronounce file
-            copy_files('book/book_sentence', word, T_SENTENCEVIDEO)
+#    if create_folder('book/book_pronounce'):
+#        for word in word_list: # copy word pronounce file
+#            copy_files('book/book_pronounce', word, T_WORDVIDEO)
+#    if create_folder('book/book_sentence'):
+#        for word in word_list: # copy sentence pronounce file
+#            copy_files('book/book_sentence', word, T_SENTENCEVIDEO)
     if create_folder('book/book_deformation'):
         for word in word_list: # copy deformation image file
             if word[T_DEFORMATION_IMG]: 
                 copy_files('book/book_deformation', word, T_DEFORMATION_IMG)
-
+    if create_folder('word/word_images'):
+        resize_all_images('book/book_images', 'word/word_images', 150)
+    if create_folder('word/word_deformation'):
+        resize_all_images('book/book_deformation', 'word/word_deformation', 150)
+        
 # Write html file
 def write_html_file():
     with open('book/book.html', mode='w', encoding='utf-8') as file:
@@ -129,63 +142,69 @@ def write_html_file():
             
             # first row
             file.write('<tr>\n')
+            
             # first column
             target_path = './book_images/{}.{}'.format(word[T_WORD], 
                     word[T_IMAGEPATH].rsplit('.', 1)[1]).replace(' ', '_');
-            file.write('<td align="center" valign="top" width="150" rowSpan="3">')
-            file.write('<img width="90%" height="auto" src="{}"></td>\n'.format(target_path))
+            file.write('<td align="center" valign="middle">')
+            file.write('<img src="{}"></td>\n'.format(target_path))
+            
             # second column
-            file.write('<td align="left" width="400"><font size="3"><b>{}</b></font> <font size="2">{} {}</font></td>'.format(
+            file.write('<td align="left" valign="middle" width="500"><font face="Microsoft Yahei" size="4"><b>{}</b></font> <font size="3">{} {}'.format(
                 word[T_WORD], word[T_PHONETIC], word[T_WORDMEAN]))
 #            if word[T_WORDMEAN_EN]:
 #                file.write('<br><英译> {}'.format(word[T_WORDMEAN_EN]))
-            # third column
-            file.write('<td align="left" valign="middle" width="150" rowSpan="3">')
-            if word[T_DEFORMATION_IMG]:
-                target_path = './book_deformation/{}.{}'.format(word[T_WORD], 
-                        word[T_DEFORMATION_IMG].rsplit('.', 1)[1]).replace(' ', '_');
-                file.write('<img width="90%" height="auto" src="{}">'.format(target_path))
-                if word[T_DEFORMATION_DESC]:
-                    file.write('<br><font size="2">{}</font>'.format(word[T_DEFORMATION_DESC]))
-            file.write('</td>\n')
-            file.write('</tr>\n')
 
-            # second row
-            file.write('<tr>\n')
-            file.write('<td align="left" valign="top" width="400"><font size="2"><例> {} {}</font></td>\n'
+            file.write('<br><font face="Microsoft Yahei">[例]</font> {} {}'
                     .format('<u><b>{}</b></u>'.format(word[T_WORD])
                     .join(re.split(word[T_WORD], word[T_SENTENCE], flags = re.IGNORECASE)), word[T_SENTENCETRANS]))
-            file.write('</tr>\n')
 
-            # third row
-            file.write('<tr>\n')
-            file.write('<td align="left" valign="top" width="400"><font size="2">')
-            have_variant = False
-            have_etyma = False
             if word[T_ETYMA]:
-                file.write('<词源> {}'.format(word[T_ETYMA]))
-                have_etyma = True
+                file.write('<br><font face="Microsoft Yahei">[词源]</font> {}'.format(word[T_ETYMA]))
+                
+            have_variant = False
             for variant in word[T_WORDVARIANTS]:
                 if variant != word[T_WORD]:
                     if not have_variant:
-                        if have_etyma:
-                            file.write(' ')
-                        file.write('<变式> {}'.format(variant))
+                        file.write('<br><font face="Microsoft Yahei">[变式]</font> {}'.format(variant))
                         have_variant = True
                     else:
                         file.write(', {}'.format(variant))
             file.write('</font></td>\n')
+            
+            # third column
+            file.write('<td align="left" valign="middle">')
+            if word[T_DEFORMATION_IMG]:
+                target_path = './book_deformation/{}.{}'.format(word[T_WORD], 
+                        word[T_DEFORMATION_IMG].rsplit('.', 1)[1]).replace(' ', '_');
+                file.write('<img src="{}">'.format(target_path))
+                if word[T_DEFORMATION_DESC]:
+                    file.write('</p><font size="3">{}</font>'.format(word[T_DEFORMATION_DESC]))
+            file.write('</td>\n')
             file.write('</tr>\n')
 
+            # second row
+            file.write('<tr><td colspan=3><font size="1"><hr size=1></font></td></tr>\n')
+            
         file.write('</table></body></html>')
 
+def create_word_file():
+    book = open('book/book.html', 'r', encoding='utf-8')
+    word = open('word/word.html', 'w', encoding='utf-8')
+    for line in book.readlines():
+        word.write(line.replace('/book_', '/word_'))
+    book.close()
+    word.close()
+    
 def generate_word_book():
     read_baicizhantopic_db()
     read_baicizhantopicwordmean_db()
     print('total {} words.'.format(len(word_list)))
     create_folder('book')
+    create_folder('word')
     convert_dict_recource()
     write_html_file()
+    create_word_file()
 
 if __name__ == '__main__':
     generate_word_book()
